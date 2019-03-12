637713acc6c948d2329777e5a04ebbdb0b4ce1c4
stef <stephane.bachelier@gmail.com>
2019-03-12 01:16:27 +0100

    feat(mongo): add mongo client helper

---
+++ mongo.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb')
+const log = require('debug')('mongo:rs')
+const chalk = require('chalk')
+
+module.exports = async url => {
+  log('Connecting to node %s', chalk.cyan(url))
+  const client = MongoClient(url)
+
+  try {
+    await client.connect()
+
+    return client
+  } catch(e) {
+    console.error(e.stack)
+    process.exit(1)
+  }
+}
