26a577ccbec0f99cbc35eed680f86166af490948
stef <stephane.bachelier@gmail.com>
2019-03-12 01:47:54 +0100

    feat(commands): add replica set commands

---
+++ commands.js
@@ -0,0 +1,30 @@
+const log = require('debug')('mongo:rs')
+const chalk = require('chalk')
+const rs = require('./rs')
+
+module.exports = (client, replicaSet) => ({
+  initiate: async () => {
+    await rs.initiate(client, replicaSet)
+  },
+
+  status: async () => {
+    let status = await rs.status(client)
+
+    if (status.codeName === 'NotYetInitialized') {
+      log('no replica set initialized')
+    }
+
+    if (status.members) {
+      log('replica set status')
+      status.members.forEach(({ name, state }, index) => {
+        log('* %s ~> %s : %s', chalk.cyan(`#${index}`), chalk.bold.red(name), chalk.green(state))
+      })
+    }
+
+    return status
+  },
+
+  url: () => {
+    console.log(rs.url(replicaSet))
+  }
+})
