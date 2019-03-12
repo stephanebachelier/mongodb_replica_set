7ecc715ad2899dce5918e80618f90d5d75aea69c
stef <stephane.bachelier@gmail.com>
2019-03-12 01:16:54 +0100

    feat(rs): add replicat set commands

---
+++ rs/url.js
@@ -0,0 +1,6 @@
+module.exports = async replicaSet => {
+  const { name, nodes} = replicaSet
+  const hosts = nodes.map(({ host }) => host).join(',')
+
+  return `mongodb://${hosts}?replicaSet=${name}`
+}
