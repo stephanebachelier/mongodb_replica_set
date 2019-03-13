5124407460b52729e1c273b1c85d922a86d6d1c9
stef <stephane.bachelier@gmail.com>
2019-03-13 09:49:43 +0100

    docs(cli): add options aliases to cli help

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const cli = require('meow')(`
   Usage
   =====
@@ -7,26 +6,26 @@ const cli = require('meow')(`
 
   Description
   ===========
-  Create a configure a MongoDB replica set environment and start all the replica set mongod processes.
+  Create & configure a MongoDB replica set environment and start all the replica set mongod processes.
 
   Options:
   -------
 
   ## Replica set options :
 
-    - name          the replica set name (default: rs)
-    - baseDir       the data directory (default: /srv/mongodb)
-    - port          the port of the first node (default: 27017)
-    - oplog         the oplogSize parameter (default: 128)
-    - nodes         the number of nodes in replica set (default: 3)
-    - ip            the listening ip MongoDB process will be bound to (default: 127.0.0.1)
+    --name, -n          replica set name (default: rs)
+    --baseDir, -b       data directory (default: /srv/mongodb)
+    --port , -p         port of the first node (default: 27017)
+    --oplog, -o         oplogSize parameter (default: 128)
+    --nodes, -c         number of nodes in replica set (default: 3)
+    --ip , -i           listening ip MongoDB process will be bound to (default: 127.0.0.1)
 
   ## Other options
 
-    - mongoPath     path to the mongo bin folder (default: none as it will use the mongo binaries from the PATH)
-    - accessLog     the access log filename (default: 'access.log')
-    - errorLog      the error log filename (default: 'error.log')
-    - delay         a default delay to wait for the server instances to be up (default: 5000)
+    - mongoPath, -m     path to the mongo bin folder (default: none as it will use the mongo binaries from the PATH)
+    --accessLog, -a     access log filename (default: 'access.log')
+    --errorLog, -e      error log filename (default: 'error.log')
+    --delay, -d         default delay to wait for the server instances to be up (default: 5000)
 
 `, {
   flags: {
